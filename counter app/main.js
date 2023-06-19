97d292845a51250889bab2d0c658af51ee208b10
vamikazvm <70892300+vamikazvm@users.noreply.github.com>
2023-06-19 15:40:06 +0530

    Add files via upload

---
+++ counter app/main.js
@@ -0,0 +1,14 @@
+const count = document.getElementById('count');
+const add = document.getElementById('add');
+const sub = document.getElementById('sub');
+const reset = document.getElementById('reset');
+
+add.addEventListener('click', () => {
+  count.innerHTML++;
+})
+sub.addEventListener('click', () => {
+  count.innerHTML--;
+})
+reset.addEventListener('click', () => {
+  count.innerHTML = 0;
+})
