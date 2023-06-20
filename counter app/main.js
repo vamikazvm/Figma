a0492fe56db5fe632370ef2374d9752d435a1ed4
vamikazvm <70892300+vamikazvm@users.noreply.github.com>
2023-06-20 09:57:24 +0530

    Add files via upload

--- counter app/main.js
+++ counter app/main.js
@@ -5,14 +5,14 @@ const reset = document.getElementById('reset');
 
 add.addEventListener('click', () => {
   count.innerHTML++;
-})
+});
 sub.addEventListener('click', () => {
-
   count.innerHTML--;
-
-})
-
+  if (count.innerHTML == -1) {
+    alert('You cannot go negative');
+  }
+});
 
 reset.addEventListener('click', () => {
   count.innerHTML = 0;
-})
+});
